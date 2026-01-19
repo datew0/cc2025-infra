f242264877c7606244f83ffc546fd3f8fa788bb0
User <user@example.com>
2026-01-19 23:40:44 +0300

    Initial Terraform template added

---
+++ yandex-cloud/terraform/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-d"
+}
+
+resource "yandex_compute_disk" "boot-disk-1" {
+  name     = "boot-disk-1"
+  type     = "network-hdd"
+  zone     = "ru-central1-d"
+  size     = "10"
+  image_id = "fd8lcd9f54ldmonh1d72"
+}
+
+resource "yandex_compute_instance" "vm" {
+  name = "weather-service"
+  platform_id = "standard-v3"
+
+  resources {
+    cores  = 2
+    core_fraction = 20
+    memory = 1
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-1.id
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("vm.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "local_file" "ansible_inventory" {
+  filename = "${path.module}/../ansible/inventory.ini"
+
+  content = <<EOF
+[vm]
+${yandex_compute_instance.vm.network_interface[0].nat_ip_address}
+
+[vm:vars]
+ansible_user=ubuntu
+ansible_ssh_private_key_file=~/.ssh/id_rsa
+EOF
+}
